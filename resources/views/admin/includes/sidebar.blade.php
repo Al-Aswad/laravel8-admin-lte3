fa237a64cf8da10da99d314e8244255dd568d0f1
Hajar Aswad <hajaraswadkom@gmail.com>
2022-05-09 09:35:45 +0700

    laravel 8 admin lte 3

---
+++ resources/views/admin/includes/sidebar.blade.php
@@ -0,0 +1,77 @@
+<div class="sidebar">
+    <!-- Sidebar Menu -->
+    <nav class="mt-4">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+
+            <li class="nav-item">
+                <a href="#" class="nav-link px-1 active">
+                    <i class="nav-icon fas fa-tachometer-alt"></i>
+                    <p>
+                        Dashboard
+                    </p>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a href="#" class="nav-link px-1">
+                    <i class="nav-icon fas fa-tachometer-alt"></i>
+                    <p>
+                        Karyawan
+                        <i class="right fas fa-angle-left"></i>
+                    </p>
+                </a>
+                <ul class="nav nav-treeview">
+                    <li class="nav-item">
+                        <a href="./index.html" class="nav-link active px-1">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>Dashboard v1</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="./index2.html" class="nav-link px-1">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>Dashboard v2</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="./index3.html" class="nav-link active px-1">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>Dashboard v3</p>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+
+            <li class="nav-item">
+                <a href="#" class="nav-link px-1">
+                    <i class="nav-icon far fa-envelope"></i>
+                    <p>
+                        Mailbox
+                        <i class="fas fa-angle-left right"></i>
+                    </p>
+                </a>
+                <ul class="nav nav-treeview">
+                    <li class="nav-item">
+                        <a href="pages/mailbox/mailbox.html" class="nav-link px-1">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>Inbox</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="pages/mailbox/compose.html" class="nav-link px-1">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>Compose</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="pages/mailbox/read-mail.html" class="nav-link px-1">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>Read</p>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+    <!-- /.sidebar-menu -->
+</div>
